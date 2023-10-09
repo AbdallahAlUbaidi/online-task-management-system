4661e26598934e8288ebaae1b7de0320a47d818b
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-10-09 21:03:18 +0300

    FEAT: Db connection function expanded for flexibility
    The function is able to accept options object as well as port number for the host of the db server

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,57 @@
 import mongoose from "mongoose";
 
+const constructConnectionString = ({
+	dbHost,
+	dbName,
+	port,
+	username,
+	password,
+	options
+}) => {
+	let connectionString = `mongodb${options && options.srv ? "+srv" : ""}://`;
+
+	if (username && password) {
+		const authCredentials = `${username}:${password}@`;
+		connectionString += authCredentials;
+	}
+
+	const hostString = `${dbHost}${port ? `:${port}` : ""}`;
+	connectionString += hostString;
+
+	const dbNameString = `/${dbName}`;
+	connectionString += dbNameString;
+
+	if (options && options instanceof Object) {
+		let optionsString = "?";
+		optionsString += Object.entries(options)
+			.map(([key, value]) => `${key}=${value}`)
+			.join("&");
+		connectionString += optionsString;
+	}
+	return connectionString;
+};
+
 export default async ({
 	dbHost,
 	dbName,
+	port,
 	username,
-	password
+	password,
+	options
 }) => {
-	const connectionString = username ?
-		`mongodb://${username}:${password}@${dbHost}:27017/${dbName}`
-		: `mongodb://${dbHost}:27017/${dbName}`;
-	return mongoose.connect(connectionString);
+	const connectionString = constructConnectionString({
+		dbHost,
+		dbName,
+		port,
+		username,
+		password,
+		options
+	});
+
+	const connection = await mongoose.connect(connectionString);
+	mongoose.connection.on("error", err => console.log(
+		`${err.name}: ${err.message}`
+	));
+
+	return connection;
 };
