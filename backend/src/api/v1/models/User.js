213d05b943a0a23179b7c986799222f196283a2c
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-10-11 19:12:27 +0300

    FEAT: created User model

---
+++ backend/src/api/v1/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.createSchema({
+	username: {
+		type: mongoose.SchemaTypes.String,
+		required: true,
+		unique: true,
+		minLength: 6,
+		maxLength: 30
+	},
+	email: {
+		type: mongoose.SchemaTypes.String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: mongoose.SchemaTypes.String,
+		required: true,
+	}
+});
+
+export default mongoose.model("User", userSchema);
