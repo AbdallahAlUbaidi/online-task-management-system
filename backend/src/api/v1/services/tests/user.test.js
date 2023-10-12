fae9f4018ed71d336903d69b8e72af07eb427423
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-10-12 11:36:37 +0300

    TEST: tested getUserByName and getUserByEmail services

--- backend/src/api/v1/services/tests/user.test.js
+++ backend/src/api/v1/services/tests/user.test.js
@@ -1,7 +1,9 @@
 import { vi, describe, it, beforeEach, expect } from "vitest";
 
 import {
-	createUser
+	createUser,
+	getUserByName,
+	getUserByEmail
 } from "../user.js";
 
 describe("createUser service", () => {
@@ -64,4 +66,177 @@ describe("createUser service", () => {
 			expect(User.create.mock.results[0].value).deep.toEqual(newUser);
 		}
 	}, 1000);
+});
+
+
+describe("getUserByName service", () => {
+	let User = {};
+	beforeEach(() => {
+		User.findOne = vi.fn(({ username }) =>
+			new Promise(resolve => {
+				setTimeout(() => {
+					if (!/existing/gi.test(username))
+						resolve(null);
+					else resolve({
+						_id: String(Math.floor(Math.random() * 100000)),
+						username,
+						email: "email",
+						password: "$hash$"
+					});
+				}, 100);
+			}));
+	});
+
+	it("should call User.findOne() with an object containing passed username", async () => {
+		let mockData = [
+			{ username: "username1" },
+			{ username: "username2" },
+			{ username: "username3" },
+		];
+
+		for (let data of mockData) {
+			User.findOne.mockClear();
+			await getUserByName({
+				...data,
+				UserModel: User
+			});
+
+			expect(User.findOne).toBeCalledTimes(1);
+			expect(typeof User.findOne.mock.calls[0][0]).toBe("object");
+			expect(User.findOne.mock.results[0].value).not.toBeInstanceOf(Promise);
+			expect(User.findOne.mock.calls[0][0].username).toBe(data.username);
+		}
+	});
+
+	it("should return a user object if a user exists", async () => {
+		let mockData = [
+			{ username: "existing username1" },
+			{ username: "existing username2" },
+			{ username: "existing username3" },
+		];
+
+		for (let data of mockData) {
+			User.findOne.mockClear();
+			const user = await getUserByName({
+				...data,
+				UserModel: User
+			});
+
+			expect(User.findOne.mock.results[0].value).not.toBeInstanceOf(Promise);
+			expect(user._id).toBeDefined();
+			expect(user.email).toBeDefined();
+			expect(user.password).toBeDefined();
+			expect(user.username).toBe(data.username);
+		}
+	}, 1000);
+
+	it("should return null if no username is passed", async () => {
+		const user = await getUserByName({ UserModel: User });
+		expect(user).toBeNull();
+	}, 1000);
+
+	it("should return null if not matching user is found", async () => {
+		let mockData = [
+			{ username: "username1" },
+			{ username: "username2" },
+			{ username: "username3" },
+		];
+
+		for (let data of mockData) {
+			User.findOne.mockClear();
+			const user = await getUserByName({
+				...data,
+				UserModel: User
+			});
+
+			expect(User.findOne.mock.results[0].value).not.toBeInstanceOf(Promise);
+			expect(user).toBeNull();
+		}
+	});
+});
+
+describe("getUserByEmail service", () => {
+	let User = {};
+	beforeEach(() => {
+		User.findOne = vi.fn(({ email }) =>
+			new Promise(resolve => {
+				setTimeout(() => {
+					if (!/existing/gi.test(email))
+						resolve(null);
+					else resolve({
+						_id: String(Math.floor(Math.random() * 100000)),
+						email,
+						username: "username",
+						password: "$hash$"
+					});
+				}, 100);
+			}));
+	});
+
+	it("should call User.findOne() with an object containing passed email", async () => {
+		let mockData = [
+			{ email: "email1" },
+			{ email: "email2" },
+			{ email: "email3" },
+		];
+
+		for (let data of mockData) {
+			User.findOne.mockClear();
+			await getUserByEmail({
+				...data,
+				UserModel: User
+			});
+
+			expect(User.findOne).toBeCalledTimes(1);
+			expect(typeof User.findOne.mock.calls[0][0]).toBe("object");
+			expect(User.findOne.mock.results[0].value).not.toBeInstanceOf(Promise);
+			expect(User.findOne.mock.calls[0][0].email).toBe(data.email);
+		}
+	});
+
+	it("should return a user object if a user exists", async () => {
+		let mockData = [
+			{ email: "existing email1" },
+			{ email: "existing email2" },
+			{ email: "existing email3" },
+		];
+
+		for (let data of mockData) {
+			User.findOne.mockClear();
+			const user = await getUserByEmail({
+				...data,
+				UserModel: User
+			});
+
+			expect(User.findOne.mock.results[0].value).not.toBeInstanceOf(Promise);
+			expect(user._id).toBeDefined();
+			expect(user.username).toBeDefined();
+			expect(user.password).toBeDefined();
+			expect(user.email).toBe(data.email);
+		}
+	}, 1000);
+
+	it("should return null if no email is passed", async () => {
+		const user = await getUserByEmail({ UserModel: User });
+		expect(user).toBeNull();
+	}, 1000);
+
+	it("should return null if not matching user is found", async () => {
+		let mockData = [
+			{ email: "email1" },
+			{ email: "email2" },
+			{ email: "email3" },
+		];
+
+		for (let data of mockData) {
+			User.findOne.mockClear();
+			const user = await getUserByEmail({
+				...data,
+				UserModel: User
+			});
+
+			expect(User.findOne.mock.results[0].value).not.toBeInstanceOf(Promise);
+			expect(user).toBeNull();
+		}
+	});
 });
