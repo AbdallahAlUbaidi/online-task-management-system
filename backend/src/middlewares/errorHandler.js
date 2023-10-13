9b7250eace42691d8eed59f64d27a309b34f327c
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-10-13 16:31:02 +0300

    FIX: next() is called after error is handled

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -3,7 +3,7 @@ import ApiError from "../api/ApiError.js";
 export default (error, req, res, next) => {
 	if (error instanceof ApiError) {
 		const { httpStatusCode, message } = error;
-		res.status(httpStatusCode).json({ message });
+		return res.status(httpStatusCode).json({ message });
 	}
 	next(error);
 };
