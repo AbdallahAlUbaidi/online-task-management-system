8671cfa94c860110d26c107029441a05fe97dc29
AbdallahAlUbaidi <abdallah.alubaidi111@gmail.com>
2023-10-15 09:33:51 +0300

    TEST: tested getTaskByUserId

--- backend/src/api/v1/services/tests/task.test.js
+++ backend/src/api/v1/services/tests/task.test.js
@@ -1,11 +1,70 @@
 import { vi, describe, expect, it, beforeEach } from "vitest";
 import { faker } from "@faker-js/faker";
 
+import ApiError from "../../../ApiError.js";
+import {
+	UNAUTHENTICATED_ERROR
+} from "../../../../constants/apiErrorCodes.js";
+
 import generateFakeMongooseId from "../../../../helpers/generateFakeMongooseId.js";
 
 import {
-	createTask
+	createTask,
+	getTasksByUserId
 } from "../task.js";
+
+describe("Get tasks by user id", () => {
+
+	let TaskModel = {};
+
+	beforeEach(() => {
+		TaskModel.find = vi.fn();
+	});
+
+	it("Should throw an api unauthenticated error when user id is undefined", async () => {
+		//Arrange
+		const userId = undefined;
+
+		//Act
+		try {
+			await getTasksByUserId({
+				userId,
+				TaskModel
+			});
+			throw new Error("Expected getTasksByUserId to throw an error but it didn't");
+		} catch (err) {
+
+			//Assert
+			expect(err).toBeInstanceOf(ApiError);
+			expect(err.errorCode).toBe(UNAUTHENTICATED_ERROR);
+		}
+
+
+	});
+
+	it("Should return an array of task objects when user id is passed", async () => {
+		//Arrange
+		const fakeUserId = generateFakeMongooseId();
+
+		const fakeTasksArr = [{
+			_id: generateFakeMongooseId(),
+			description: faker.lorem.sentence(),
+			userId: fakeUserId
+		}];
+
+		TaskModel.find.mockImplementationOnce(() =>
+			Promise.resolve(fakeTasksArr));
+
+		//Act
+		const tasks = await getTasksByUserId({
+			userId: fakeUserId,
+			TaskModel
+		});
+
+		//Assert
+		expect(tasks).toStrictEqual(fakeTasksArr);
+	});
+});
 
 describe("Create task service", () => {
 	let TaskModel = {};
